50ab625cb0997ebe73667de9d11ebe92ed7fa9cb
karthikramasamyppm <36290059+karthikramasamyppm@users.noreply.github.com>
2019-09-20 17:40:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const { mongoose } = require('./db.js');
 var employeeController = require('./controllers/employeeController.js');
 
 var app = express();
-var port     = process.env.PORT || 3000
+//var port     = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
+
 app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:4200' }));
 
-app.listen(port, () => console.log('Server started at port : 3000'));
+app.listen(port, () => console.log('Server started at port :'+port));
 
 
 app.use('/employees', employeeController);
